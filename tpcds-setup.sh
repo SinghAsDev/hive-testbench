7664e99af8d8c8a28ec02b2558ad4a8daba249cc
Brock Noland <brock@apache.org>
2014-11-28 22:03:06 -0800

    generate only some tables

--- tpcds-setup.sh
+++ tpcds-setup.sh
@@ -20,8 +20,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # Tables in the TPC-DS schema.
-DIMS="date_dim time_dim item customer customer_demographics household_demographics customer_address store promotion warehouse ship_mode reason income_band call_center web_page catalog_page web_site"
-FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_returns inventory"
+DIMS="date_dim time_dim customer customer_address customer_demographics household_demographics item promotion store"
+FACTS="store_sales"
 
 # Get the parameters.
 SCALE=$1
