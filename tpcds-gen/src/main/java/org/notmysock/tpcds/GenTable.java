cb4e7a277ffa476819aebd43ca8a7412ca57c115
Brock Noland <brock@apache.org>
2014-11-28 21:29:52 -0800

    Generate tpcds without writing to FS first

--- tpcds-gen/src/main/java/org/notmysock/tpcds/GenTable.java
+++ tpcds-gen/src/main/java/org/notmysock/tpcds/GenTable.java
@@ -1,218 +1,224 @@
 package org.notmysock.tpcds;
 
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.fs.*;
-import org.apache.hadoop.hdfs.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.util.*;
-import org.apache.hadoop.filecache.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.mapreduce.lib.reduce.*;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.net.URI;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
 
-import org.apache.commons.cli.*;
-import org.apache.commons.*;
-
-import java.io.*;
-import java.nio.*;
-import java.util.*;
-import java.net.*;
-import java.math.*;
-import java.security.*;
-
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class GenTable extends Configured implements Tool {
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        int res = ToolRunner.run(conf, new GenTable(), args);
-        System.exit(res);
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    int res = ToolRunner.run(conf, new GenTable(), args);
+    System.exit(res);
+  }
+
+  @SuppressWarnings("deprecation")
+  @Override
+  public int run(String[] args) throws Exception {
+    String[] remainingArgs = new GenericOptionsParser(getConf(), args)
+        .getRemainingArgs();
+
+    CommandLineParser parser = new BasicParser();
+    getConf().setInt("io.sort.mb", 4);
+    org.apache.commons.cli.Options options = new org.apache.commons.cli.Options();
+    options.addOption("s", "scale", true, "scale");
+    options.addOption("t", "table", true, "table");
+    options.addOption("d", "dir", true, "dir");
+    options.addOption("p", "parallel", true, "parallel");
+    CommandLine line = parser.parse(options, remainingArgs);
+
+    if (!(line.hasOption("scale") && line.hasOption("dir"))) {
+      HelpFormatter f = new HelpFormatter();
+      f.printHelp("GenTable", options);
+      return 1;
     }
 
-    @Override
-    public int run(String[] args) throws Exception {
-        String[] remainingArgs = new GenericOptionsParser(getConf(), args).getRemainingArgs();
+    int scale = Integer.parseInt(line.getOptionValue("scale"));
+    String table = "all";
+    if (line.hasOption("table")) {
+      table = line.getOptionValue("table");
+    }
+    Path out = new Path(line.getOptionValue("dir"));
 
-        CommandLineParser parser = new BasicParser();
-        getConf().setInt("io.sort.mb", 4);
-        org.apache.commons.cli.Options options = new org.apache.commons.cli.Options();
-        options.addOption("s","scale", true, "scale");
-        options.addOption("t","table", true, "table");
-        options.addOption("d","dir", true, "dir");
-        options.addOption("p", "parallel", true, "parallel");
-        CommandLine line = parser.parse(options, remainingArgs);
+    int parallel = scale;
 
-        if(!(line.hasOption("scale") && line.hasOption("dir"))) {
-          HelpFormatter f = new HelpFormatter();
-          f.printHelp("GenTable", options);
-          return 1;
-        }
-        
-        int scale = Integer.parseInt(line.getOptionValue("scale"));
-        String table = "all";
-        if(line.hasOption("table")) {
-          table = line.getOptionValue("table");
-        }
-        Path out = new Path(line.getOptionValue("dir"));
-
-        int parallel = scale;
-
-        if(line.hasOption("parallel")) {
-          parallel = Integer.parseInt(line.getOptionValue("parallel"));
-        }
-
-        if(parallel == 1 || scale == 1) {
-          System.err.println("The MR task does not work for scale=1 or parallel=1");
-          return 1;
-        }
-
-        Path in = genInput(table, scale, parallel);
-
-        Path dsdgen = copyJar(new File("target/lib/dsdgen.jar"));
-        URI dsuri = dsdgen.toUri();
-        URI link = new URI(dsuri.getScheme(),
-                    dsuri.getUserInfo(), dsuri.getHost(), 
-                    dsuri.getPort(),dsuri.getPath(), 
-                    dsuri.getQuery(),"dsdgen");
-        Configuration conf = getConf();
-        conf.setInt("mapred.task.timeout",0);
-        conf.setInt("mapreduce.task.timeout",0);
-        conf.setBoolean("mapreduce.map.output.compress", true);
-        conf.set("mapreduce.map.output.compress.codec", "org.apache.hadoop.io.compress.GzipCodec");
-        DistributedCache.addCacheArchive(link, conf);
-        DistributedCache.createSymlink(conf);
-        Job job = new Job(conf, "GenTable+"+table+"_"+scale);
-        job.setJarByClass(getClass());
-        job.setNumReduceTasks(0);
-        job.setMapperClass(DSDGen.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-
-        job.setInputFormatClass(NLineInputFormat.class);
-        NLineInputFormat.setNumLinesPerSplit(job, 1);
-
-        FileInputFormat.addInputPath(job, in);
-        FileOutputFormat.setOutputPath(job, out);
-
-        // use multiple output to only write the named files
-        LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
-        MultipleOutputs.addNamedOutput(job, "text", 
-          TextOutputFormat.class, LongWritable.class, Text.class);
-
-        boolean success = job.waitForCompletion(true);
-
-        // cleanup
-        FileSystem fs = FileSystem.get(getConf());
-        
-        fs.delete(in, false);
-        fs.delete(dsdgen, false);
-
-        return 0;
+    if (line.hasOption("parallel")) {
+      parallel = Integer.parseInt(line.getOptionValue("parallel"));
     }
 
-    public Path copyJar(File jar) throws Exception {
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      InputStream is = new FileInputStream(jar);
-      try {
-        is = new DigestInputStream(is, md);
-        // read stream to EOF as normal...
-      }
-      finally {
-        is.close();
-      }
-      BigInteger md5 = new BigInteger(md.digest()); 
-      String md5hex = md5.toString(16);
-      Path dst = new Path(String.format("/tmp/%s.jar",md5hex));
-      Path src = new Path(jar.toURI());
-      FileSystem fs = FileSystem.get(getConf());
-      fs.copyFromLocalFile(false, /*overwrite*/true, src, dst);
-      return dst; 
+    if (parallel == 1 || scale == 1) {
+      System.err.println("The MR task does not work for scale=1 or parallel=1");
+      return 1;
     }
 
-    public Path genInput(String table, int scale, int parallel) throws Exception {
-        long epoch = System.currentTimeMillis()/1000;
+    Path in = genInput(table, scale, parallel);
 
-        Path in = new Path("/tmp/"+table+"_"+scale+"-"+epoch);
-        FileSystem fs = FileSystem.get(getConf());
-        FSDataOutputStream out = fs.create(in);
-        for(int i = 1; i <= parallel; i++) {
-          if(table.equals("all")) {
-            out.writeBytes(String.format("./dsdgen -dir $DIR -force Y -scale %d -parallel %d -child %d\n", scale, parallel, i));
-          } else {
-            out.writeBytes(String.format("./dsdgen -dir $DIR -table %s -force Y -scale %d -parallel %d -child %d\n", table, scale, parallel, i));
-          }
-        }
-        out.close();
-        return in;
+    Path dsdgen = copyJar(new File("target/lib/dsdgen.jar"));
+    URI dsuri = dsdgen.toUri();
+    URI link = new URI(dsuri.getScheme(), dsuri.getUserInfo(), dsuri.getHost(),
+        dsuri.getPort(), dsuri.getPath(), dsuri.getQuery(), "dsdgen");
+    Configuration conf = getConf();
+    conf.setInt("mapred.task.timeout", 0);
+    conf.setInt("mapreduce.task.timeout", 0);
+    conf.setBoolean("mapreduce.map.output.compress", true);
+    conf.set("mapreduce.map.output.compress.codec",
+        "org.apache.hadoop.io.compress.GzipCodec");
+    DistributedCache.addCacheArchive(link, conf);
+    DistributedCache.createSymlink(conf);
+    Job job = new Job(conf, "GenTable+" + table + "_" + scale);
+    job.setJarByClass(getClass());
+    job.setNumReduceTasks(0);
+    job.setMapperClass(DSDGen.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+
+    job.setInputFormatClass(NLineInputFormat.class);
+    NLineInputFormat.setNumLinesPerSplit(job, 1);
+
+    FileInputFormat.addInputPath(job, in);
+    FileOutputFormat.setOutputPath(job, out);
+
+    // use multiple output to only write the named files
+    LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
+    MultipleOutputs.addNamedOutput(job, "text", TextOutputFormat.class,
+        LongWritable.class, Text.class);
+
+    boolean success = job.waitForCompletion(true);
+
+    // cleanup
+    FileSystem fs = FileSystem.get(getConf());
+    fs.delete(in, false);
+    fs.delete(dsdgen, false);
+
+    return success ? 0 : 1;
+  }
+
+  public Path copyJar(File jar) throws Exception {
+    MessageDigest md = MessageDigest.getInstance("MD5");
+    InputStream is = new FileInputStream(jar);
+    try {
+      is = new DigestInputStream(is, md);
+      // read stream to EOF as normal...
+    } finally {
+      is.close();
+    }
+    BigInteger md5 = new BigInteger(md.digest());
+    String md5hex = md5.toString(16);
+    Path dst = new Path(String.format("/tmp/%s.jar", md5hex));
+    Path src = new Path(jar.toURI());
+    FileSystem fs = FileSystem.get(getConf());
+    fs.copyFromLocalFile(false, /* overwrite */true, src, dst);
+    return dst;
+  }
+
+  public Path genInput(String table, int scale, int parallel) throws Exception {
+    long epoch = System.currentTimeMillis() / 1000;
+
+    Path in = new Path("/tmp/" + table + "_" + scale + "-" + epoch);
+    FileSystem fs = FileSystem.get(getConf());
+    FSDataOutputStream out = fs.create(in);
+    for (int i = 1; i <= parallel; i++) {
+      if (table.equals("all")) {
+        out.writeBytes(String.format("all %d %d %d\n", scale, parallel, i));
+        // out.writeBytes(String.format("./dsdgen -dir $DIR -force Y -scale %d -parallel %d -child %d\n",
+        // scale, parallel, i));
+      } else {
+        out.writeBytes(String
+            .format("%s %d %d %d\n", table, scale, parallel, i));
+        // out.writeBytes(String.format("./dsdgen -dir $DIR -table %s -force Y -scale %d -parallel %d -child %d\n",
+        // table, scale, parallel, i));
+      }
+    }
+    out.close();
+    return in;
+  }
+
+  static final class DSDGen extends Mapper<LongWritable, Text, Text, Text> {
+    private MultipleOutputs<Text, Text> mos;
+
+    protected void setup(Context context) throws IOException {
+      mos = new MultipleOutputs<Text, Text>(context);
     }
 
-    static String readToString(InputStream in) throws IOException {
-      InputStreamReader is = new InputStreamReader(in);
-      StringBuilder sb=new StringBuilder();
-      BufferedReader br = new BufferedReader(is);
-      String read = br.readLine();
-
-      while(read != null) {
-        //System.out.println(read);
-        sb.append(read);
-        read =br.readLine();
-      }
-      return sb.toString();
+    protected void cleanup(Context context) throws IOException,
+        InterruptedException {
+      mos.close();
     }
 
-    static final class DSDGen extends Mapper<LongWritable,Text, Text, Text> {
-      private MultipleOutputs mos;
-      protected void setup(Context context) throws IOException {
-        mos = new MultipleOutputs(context);
-      }
-      protected void cleanup(Context context) throws IOException, InterruptedException {
-        mos.close();
-      }
-      protected void map(LongWritable offset, Text command, Mapper.Context context) 
+    protected void map(LongWritable offset, Text command, Context context)
         throws IOException, InterruptedException {
-        String parallel="1";
-        String child="1";
 
-        String[] cmd = command.toString().split(" ");
+      String[] args = command.toString().split(" ");
+      String table = args[0];
+      String scale = args[1];
+      String parallel = args[2];
+      String child = args[3];
+      String cmd = String.format("./dsdgen -TABLE %s -FORCE Y -SCALE %d -PARALLEL %d -CHILD %d -QUIET Y -FILTER Y",
+          table, scale, parallel, child);
+      final Process p = Runtime.getRuntime().exec(cmd, null,
+          new File("dsdgen/tools/"));
+      
+      Thread stderrReader = new Thread() {
+        public void run() {
+          try {
+            InputStreamReader is = new InputStreamReader(p.getErrorStream());
+            BufferedReader br = new BufferedReader(is);
+            String read;
 
-        for(int i=0; i<cmd.length; i++) {
-          if(cmd[i].equals("$DIR")) {
-            cmd[i] = (new File(".")).getAbsolutePath();
-          }
-          if(cmd[i].equals("-parallel")) {
-            parallel = cmd[i+1];
-          }
-          if(cmd[i].equals("-child")) {
-            child = cmd[i+1];
+            while ((read = br.readLine()) != null) {
+              System.err.println(read);
+            }
+          } catch (EOFException e) {
+            // ignore
+          } catch (Exception e) {
+            e.printStackTrace();
           }
         }
-
-        Process p = Runtime.getRuntime().exec(cmd, null, new File("dsdgen/tools/"));
-        int status = p.waitFor();
-        if(status != 0) {
-          String err = readToString(p.getErrorStream());
-          throw new InterruptedException("Process failed with status code " + status + "\n" + err);
-        }
-
-        File cwd = new File(".");
-        final String suffix = String.format("_%s_%s.dat", child, parallel);
-
-        FilenameFilter tables = new FilenameFilter() {
-          public boolean accept(File dir, String name) {
-            return name.endsWith(suffix);
-          }
-        };
-
-        for(File f: cwd.listFiles(tables)) {
-          BufferedReader br = new BufferedReader(new FileReader(f));          
-          String line;
-          while ((line = br.readLine()) != null) {
-            // process the line.
-            mos.write("text", line, null, f.getName().replace(suffix,"/data"));
-          }
-          br.close();
-          f.deleteOnExit();
-        }
+      };
+      stderrReader.setName("stderrReader");
+      stderrReader.setDaemon(true);
+      stderrReader.start();
+      BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String line;
+      while ((line = br.readLine()) != null) {
+        mos.write("text", line, null, table);
+      }
+      br.close();
+      int status = p.waitFor();
+      if (status != 0) {
+        throw new InterruptedException("Process failed with status code " + status);
       }
     }
+  }
 }
--- tpcds-setup.sh
+++ tpcds-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 function usage {
-	echo "Usage: tpcds-setup.sh scale_factor [temp_directory]"
-	exit 1
+  echo "Usage: tpcds-setup.sh scale_factor [temp_directory]"
+  exit 1
 }
 
 function runcommand {
@@ -10,13 +10,13 @@ function runcommand {
 }
 
 if [ ! -f tpcds-gen/target/tpcds-gen-1.0-SNAPSHOT.jar ]; then
-	echo "Please build the data generator with ./tpcds-build.sh first"
-	exit 1
+  echo "Please build the data generator with ./tpcds-build.sh first"
+  exit 1
 fi
 which hive > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Script must be run where Hive is installed"
-	exit 1
+  echo "Script must be run where Hive is installed"
+  exit 1
 fi
 
 # Tables in the TPC-DS schema.
@@ -27,38 +27,40 @@ FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_ret
 SCALE=$1
 DIR=$2
 if [ "X$BUCKET_DATA" != "X" ]; then
-	BUCKETS=13
-	RETURN_BUCKETS=13
+  BUCKETS=13
+  RETURN_BUCKETS=13
 else
-	BUCKETS=1
-	RETURN_BUCKETS=1
+  BUCKETS=1
+  RETURN_BUCKETS=1
 fi
 if [ "X$DEBUG_SCRIPT" != "X" ]; then
-	set -x
+  set -x
 fi
 
 # Sanity checking.
 if [ X"$SCALE" = "X" ]; then
-	usage
+  usage
 fi
 if [ X"$DIR" = "X" ]; then
-	DIR=/user/hive/warehouse/tpcds_text_${SCALE}.db
+  DIR=/user/hive/warehouse/tpcds_text_${SCALE}.db
 fi
 if [ $SCALE -eq 1 ]; then
-	echo "Scale factor must be greater than 1"
-	exit 1
+  echo "Scale factor must be greater than 1"
+  exit 1
 fi
 
 # Do the actual data load.
-hdfs dfs -ls ${DIR}/ > /dev/null
-if [ $? -ne 0 ]; then
-	echo "Generating data at scale factor $SCALE."
+f hdfs dfs -ls ${DIR}/ > /dev/null
+  echo "Generating data at scale factor $SCALE."
   pushd tpcds-gen
-	if ! hadoop jar target/*.jar -d ${DIR}/ -s ${SCALE}
-  then
-    echo "Data generation failed, exiting."
-    exit 1
-  fi
+  for t in ${FACTS} ${DIMS}
+  do
+    if ! hadoop jar target/*.jar -d ${DIR}/ -s ${SCALE} -t $t
+    then
+      echo "Data generation failed, exiting."
+      exit 1
+    fi
+  done
   popd
   echo "TPC-DS text data generation complete."
 fi
@@ -69,39 +71,39 @@ runcommand "hive -i settings/load-flat.sql -f ddl-tpcds/text/alltables.sql -d DB
 
 # Create the partitioned and bucketed tables.
 if [ "X$FORMAT" = "X" ]; then
-	FORMAT=orc
+  FORMAT=orc
 fi
 i=1
 total=24
 DATABASE=tpcds_bin_partitioned_${FORMAT}_${SCALE}
 for t in ${FACTS}
 do
-	echo "Optimizing table $t ($i/$total)."
-	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
-	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
-	    -d SOURCE=tpcds_text_${SCALE} -d BUCKETS=${BUCKETS} \
-	    -d RETURN_BUCKETS=${RETURN_BUCKETS} -d FILE=${FORMAT}"
-	runcommand "$COMMAND"
-	if [ $? -ne 0 ]; then
-		echo "Command failed, try 'export DEBUG_SCRIPT=ON' and re-running"
-		exit 1
-	fi
-	i=`expr $i + 1`
+  echo "Optimizing table $t ($i/$total)."
+  COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+      -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
+      -d SOURCE=tpcds_text_${SCALE} -d BUCKETS=${BUCKETS} \
+      -d RETURN_BUCKETS=${RETURN_BUCKETS} -d FILE=${FORMAT}"
+  runcommand "$COMMAND"
+  if [ $? -ne 0 ]; then
+    echo "Command failed, try 'export DEBUG_SCRIPT=ON' and re-running"
+    exit 1
+  fi
+  i=`expr $i + 1`
 done
 
 # Populate the smaller tables.
 for t in ${DIMS}
 do
-	echo "Optimizing table $t ($i/$total)."
-	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
-	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} -d SOURCE=tpcds_text_${SCALE} \
-	    -d FILE=${FORMAT}"
-	runcommand "$COMMAND"
-	if [ $? -ne 0 ]; then
-		echo "Command failed, try 'export DEBUG_SCRIPT=ON' and re-running"
-		exit 1
-	fi
-	i=`expr $i + 1`
+  echo "Optimizing table $t ($i/$total)."
+  COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+      -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} -d SOURCE=tpcds_text_${SCALE} \
+      -d FILE=${FORMAT}"
+  runcommand "$COMMAND"
+  if [ $? -ne 0 ]; then
+    echo "Command failed, try 'export DEBUG_SCRIPT=ON' and re-running"
+    exit 1
+  fi
+  i=`expr $i + 1`
 done
 
 echo "Data loaded into database ${DATABASE}."
